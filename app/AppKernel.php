f43a336928d7231c099826575adafdc71a71d1cc
Serhii Kosyouk <monstrpro@gmail.com>
2017-10-26 22:02:25 +0300

    Add simple curd

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -15,7 +15,7 @@ class AppKernel extends Kernel
             new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
-            new AppBundle\AppBundle(),
+            new Mix\BlogBundle\MixBlogBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
